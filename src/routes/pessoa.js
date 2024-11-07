d265a327e0eac0e38bfd0514a430e37ea077a9bb
Sávio L <saviolopesgoma@gmail.com>
2024-11-06 21:27:03 -0300

    api - edit - 1

---
+++ src/routes/pessoa.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const controllerPessoa = require('../controllers/pessoa')
+const auth = require("../middleware/auth")
+
+// Inicializando as rotas do express
+const router = express.Router()
+
+// Criando as rotas
+router.post('/', controllerPessoa.CreatePessoa)
+router.post('/login', controllerPessoa.Login)
+
+router.get('/', auth, controllerPessoa.GetPessoas)
+router.put('/:id', auth, controllerPessoa.UpdatePessoa)
+router.delete('/:id', auth, controllerPessoa.DeletePessoa)
+
+// Exportar as rotas
+module.exports = router
